b23bd951bed9f352d10c03cfb9ea63ae1eeb6d9b
steve-h <stevelist@hume.ca>
2022-06-06 19:01:26 -0400

    force macos12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CustomPagesPublishPlugin",
-//    platforms: [.macOS(.v12)], // Temporary until Xcode 13.2 has been released
+   platforms: [.macOS(.v12)], // Temporary until Xcode 13.2 has been released
 
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
